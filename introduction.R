0e5aab35911e43ae7539d971e3f373ef49f4f910
Heriberto <heribertocartas@gmail.com>
2021-07-03 02:54:38 -0500

    Agrege las operacioness aritméticas básicas

--- introduction.R
+++ introduction.R
@@ -1,1 +1,24 @@
-# Bienvenidos a un breve curso introductorio del lenguaje R
+
+# Curso de R y Rstudio ----------------------------------------------------
+
+# Usando R como una calculadora -------------------------------------------
+
+# Operaciones artiméticas en R y orden de resolución (PEMDAS):
+
+# Parentesís
+(1+4)*2
+
+# Exponencial
+5^2
+
+# Multiplicación
+2*3
+
+# División
+10/5
+
+# Adición
+4+1
+
+# Sustracción
+12-6
