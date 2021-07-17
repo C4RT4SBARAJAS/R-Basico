80a9fe3509a7b0b975a6a6ad19f868fb08828e9c
Heriberto <heribertocartas@gmail.com>
2021-07-16 21:50:36 -0500

    Agregué operaciones con cadena de caracteres, status sin terminar

--- curso.R
+++ curso.R
@@ -50,11 +50,43 @@ var3 <- TRUE
 var3 <- FALSE
 
 # Operaciones con cadena de caracteres ------------------------------------
+# Prerequisitos: 
+# Intalar la librería "tidyverse" para manipular cadenas sino se tiene instalado.
+install.packages('tidyverse')
+
+# Cargar la librería "tidyverse" para poder manipular cadenas.
+library(tidyverse)
+
+# NOTA: En caso de no poder intalar la librería no te preocupes, R te permite usarla de todas maneras usando la siguente sintaxis .. 
+# nombre_libreria::funtion(argumentos), para nuestro ejemplo futuro se vería así ..
+# stringr::str_c(nombre, apellido1, apellido2)
+
 # Definimos tres variables donde la primera guradará el nombre de una persona, la segunda el primer apellido y la tercera el segundo apellido.
 nombre <- "heriberto"
 apellido1 <- "cartas"
 apellido2 <- "barrera"
 
-# Podemos sumar caracteres logrando formar cadenas:
-completo <- c(nombre, apellido1)
-completo
+# Podemos sumar caracteres logrando formar cadenas utilizndo la función str_c():
+# Llamando a la librería sin instalar ..
+stringr::str_c(nombre, apellido1, apellido2, sep = " ")
+
+# Utilizando la librería ya instalada:
+str_c(nombre, apellido1, apellido2, sep = " ")
+
+# Guardando el string en una variable:
+nombre_completo <- stringr::str_c(nombre, apellido1, apellido2, sep = " ")
+
+# Eliminar espacios basura al inicio o final:
+stringr::str_replace_all(nombre_completo, " ", "")
+
+# Sobreescribiendo la variable nombre_completo:
+nombre_completo <- stringr::str_replace_all(nombre_completo, " ", "")
+
+# Cambiar el texto a mayusculas:
+stringr::str_to_upper(nombre_completo)
+
+# Cambiar el texto a minusculas:
+stringr::str_to_lower(nombre_completo)
+
+# Longitud de una cadena con la función str_length():
+stringr::str_length(nombre_completo)
